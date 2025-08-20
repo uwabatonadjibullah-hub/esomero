50d59f5841829973c3eaf7b1c101f9f4e659289a
Nadjibullah Uwabato <nadjibullah@ksp.com>
2025-08-20 19:12:34 +0200

    Refactor routing: removed AppRouter.jsx, integrated routes directly into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-import AppRouter from './routes/AppRouter';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Welcome from './pages/Welcome';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <div className="App">
-      <AppRouter />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
