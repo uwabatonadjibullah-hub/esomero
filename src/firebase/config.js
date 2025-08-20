1d0c6c7b10bafbb622a1c53d9e0add63e0a0a6e0
Nadjibullah Uwabato <nadjibullah@ksp.com>
2025-08-20 18:41:42 +0200

    Fix Firebase auth export

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,8 @@
+// firebase/config.js
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCH1AKY9k0-Vu78knRgpgiYWDRJ7qBZ-ok",
@@ -11,5 +15,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-export default app;
+export { app, auth, db, storage };
