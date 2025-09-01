e3180423d0a1344c18d89a1d9a1d7e0e4657f164
Nadjibullah Uwabato <nadjibullah@ksp.com>
2025-09-01 09:31:23 +0200

    ALL IN

--- src/components/QuizPerformance.jsx
+++ src/components/QuizPerformance.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../firebase/config';
-import '../styles/QuizPerformance';
+import '../styles/QuizPerformance.css';
 
 const QuizPerformance = () => {
   const [quizData, setQuizData] = useState([]);
